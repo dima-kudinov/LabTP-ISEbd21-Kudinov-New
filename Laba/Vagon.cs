9fca0fc8cf4b205e7340ec08950364a611eecd09
dima-kudinov <dima-kudinov@mail.ru>
2019-12-26 20:46:04 +0400

    ///

--- Laba/Vagon.cs
+++ Laba/Vagon.cs
@@ -26,13 +26,7 @@ namespace Laba
             Posy = posY;
         }
 
-        public enum VagonCount
-        {
-            NULL,
-            ONE,
-            TWO,
-            THREE
-        }
+        
 
         private int CountToInt(VagonCount vagonCount)
         {
